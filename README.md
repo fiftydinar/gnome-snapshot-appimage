001d78012493c80630c1d68430cbbed3e96ba6e0
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-08-16 23:11:33 +0200

    Switch to new template

--- README.md
+++ README.md
@@ -2,10 +2,8 @@
 
 Test of Gnome Snapshot AppImage, not intended for daily-driving yet.
 
+When the known issue gets solved, repo will be transfered to pkgforge-dev repo & it will be the part AnyLinux-AppImages collection.
+
 ## Known issue
 
 - Opening the gallery doesn't work, something related to `glycin`
-
-## TO-DO
-
-- Need to refactor it to use newer template & to not bundle `mesa`
--- gnome-snapshot-appimage.sh
+++ gnome-snapshot-appimage.sh
@@ -2,97 +2,48 @@
 
 set -eux
 
+ARCH="$(uname -m)"
 PACKAGE=snapshot
-DESKTOP=org.gnome.Snapshot.desktop
-ICON=org.gnome.Snapshot.svg
+URUNTIME="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/uruntime2appimage.sh"
+SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
 
-export ARCH="$(uname -m)"
-export APPIMAGE_EXTRACT_AND_RUN=1
-export VERSION=$(pacman -Q "$PACKAGE" | awk 'NR==1 {print $2; exit}')
-echo "$VERSION" > ~/version
+VERSION=$(pacman -Q "$PACKAGE" | awk 'NR==1 {print $2; exit}')
+[ -n "$VERSION" ] && echo "$VERSION" > ~/version
 
-UPINFO="gh-releases-zsync|$(echo "$GITHUB_REPOSITORY" | tr '/' '|')|latest|*$ARCH.AppImage.zsync"
-SHARUN="https://github.com/VHSgunzo/sharun/releases/latest/download/sharun-$ARCH-aio"
-URUNTIME="https://github.com/VHSgunzo/uruntime/releases/latest/download/uruntime-appimage-dwarfs-$ARCH"
+# Variables used by quick-sharun
+export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
+export OUTNAME="$PACKAGE"-"$VERSION"-anylinux-"$ARCH".AppImage
+export DESKTOP=/usr/share/applications/org.gnome.Snapshot.desktop
+export ICON=/usr/share/icons/hicolor/scalable/apps/org.gnome.Snapshot.svg
+export PATH_MAPPING_RELATIVE=1 # GTK applications are usually hardcoded to look into /usr/share, especially noticeable in non-working locale
+export DEPLOY_LOCALE=1
 
 # Prepare AppDir
 mkdir -p ./AppDir/shared/lib
-cd ./AppDir
 
-# Copy desktop file & icon
-cp -v /usr/share/applications/"$DESKTOP"             ./
-cp -v /usr/share/icons/hicolor/scalable/apps/"$ICON" ./
-cp -v /usr/share/icons/hicolor/scalable/apps/"$ICON" ./.DirIcon
+# DEPLOY ALL LIBS
+wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
+chmod +x ./quick-sharun
+GSK_RENDERER=cairo ./quick-sharun /usr/bin/snapshot
+
+## Copy the icon to AppDir's share, as it's not copied by default
+mkdir -p           ./AppDir/share/icons/hicolor/scalable/apps/
+cp -v "$ICON"      ./AppDir/"${ICON#/usr/}"
 
 # Patch StartupWMClass to work on X11
 # Doesn't work when ran in Wayland, as it's 'org.gnome.Snapshot' instead.
 # It needs to be manually changed by the user in this case.
 sed -i '/^\[Desktop Entry\]/a\
 StartupWMClass=snapshot
-' "$DESKTOP"
+' ./AppDir/*.desktop
 
-# ADD LIBRARIES
-wget "$SHARUN" -O ./sharun-aio
-chmod +x ./sharun-aio
-xvfb-run -a -- ./sharun-aio l -p -v -e -s -k \
-	/usr/bin/snapshot \
-	/usr/lib/libgst* \
-	/usr/lib/gstreamer-*/*.so \
-	/usr/lib/pipewire-0.3/* \
-	/usr/lib/spa-0.2/*/* \
-        /usr/lib/libglycin*
-rm -f ./sharun-aio
+## Further debloat locale
+find ./AppDir/share/locale -type f ! -name '*glib*' ! -name '*snapshot*' -delete
 
-# Copy locale manually, as sharun doesn't do that at the moment
-cp -vr /usr/lib/locale           ./shared/lib
-cp -r /usr/share/locale          ./share
-find ./share/locale -type f ! -name '*glib*' ! -name '*snapshot*' -delete
-find ./share/locale -type f 
-# Fix hardcoded path for locale
-sed -i 's|/usr/share|././/share|g' ./shared/bin/snapshot
-echo 'SHARUN_WORKING_DIR=${SHARUN_DIR}' >> ./.env 
-
-# Manually copy snapshot gresource file, since sharun didn't copy it
-cp -rv /usr/share/snapshot/ ./share/
-
-# Deploy Gstreamer binaries manually, as sharun can only handle libraries in /lib/ for now
-echo "Deploying Gstreamer & glycin binaries..."
-cp -vn /usr/lib/gstreamer-*/*  ./shared/lib/gstreamer-* || true
-# Manually copy glycin loaders, for gallery to work
-cp -rv /usr/lib/glycin-loaders ./shared/lib
-cp -rv /usr/share/glycin-loaders/ ./share
-# Patch glycin config to look in right libraries
-sed -i 's|/usr/lib|././/lib|g' ./share/glycin-loaders/*/*/*
-
-echo "Sharunning Gstreamer & glycin bins..."
-bins_to_find="$(find ./shared/lib/ -exec file {} \; | grep -i 'elf.*executable' | awk -F':' '{print $1}')"
-for bin in $bins_to_find; do
-	mv -v "$bin" ./shared/bin && ln ./sharun "$bin"
-	echo "Sharan $bin"
-done
-
-# Prepare sharun
-ln ./sharun ./AppRun
-./sharun -g
+## Force use of cairo backend
+echo "GSK_RENDERER=cairo" >> ./AppDir/.env
 
 # MAKE APPIMAGE WITH URUNTIME
-cd ..
-wget -q "$URUNTIME" -O ./uruntime
-chmod +x ./uruntime
-
-#Add update info to runtime
-echo "Adding update information \"$UPINFO\" to runtime..."
-./uruntime --appimage-addupdinfo "$UPINFO"
-
-echo "Generating AppImage..."
-./uruntime --appimage-mkdwarfs -f \
-	--set-owner 0 --set-group 0 \
-	--no-history --no-create-timestamp \
-	--compression zstd:level=22 -S26 -B8 \
-	--header uruntime \
-	-i ./AppDir -o "$PACKAGE"-"$VERSION"-anylinux-"$ARCH".AppImage
-
-echo "Generating zsync file..."
-zsyncmake *.AppImage -u *.AppImage
-
-echo "All Done!"
+wget --retry-connrefused --tries=30 "$URUNTIME" -O ./uruntime2appimage
+chmod +x ./uruntime2appimage
+./uruntime2appimage
