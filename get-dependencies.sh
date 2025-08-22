1ee90dc716c08ef6173a9361e95d6a32ef4abdcb
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-08-22 22:17:02 +0200

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -15,7 +15,6 @@ fi
 LIBXML2_URL="https://github.com/pkgforge-dev/archlinux-pkgs-debloated/releases/download/continuous/libxml2-mini-$PKG_TYPE"
 OPUS_URL="https://github.com/pkgforge-dev/archlinux-pkgs-debloated/releases/download/continuous/opus-mini-$PKG_TYPE"
 MESA_URL="https://github.com/pkgforge-dev/archlinux-pkgs-debloated/releases/download/continuous/mesa-nano-$PKG_TYPE"
-INTEL_MEDIA_DRIVER_URL="https://github.com/pkgforge-dev/archlinux-pkgs-debloated/releases/download/continuous/intel-media-driver-mini-$PKG_TYPE" 
 GTK4_URL="https://github.com/pkgforge-dev/archlinux-pkgs-debloated/releases/download/continuous/gtk4-mini-$PKG_TYPE"
 
 echo "Installing build dependencies for sharun & AppImage integration..."
@@ -36,13 +35,10 @@ pacman -Syu --noconfirm \
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-wget --retry-connrefused --tries=30 "$LIBXML2_URL"  -O  ./"libxml2-mini-$PKG_TYPE"
+wget --retry-connrefused --tries=30 "$LIBXML2_URL" -O  ./"libxml2-mini-$PKG_TYPE"
 wget --retry-connrefused --tries=30 "$OPUS_URL"    -O  ./"opus-mini-$PKG_TYPE"
-wget --retry-connrefused --tries=30 "$MESA_URL"        -O  ./"mesa-nano-$PKG_TYPE"
-wget --retry-connrefused --tries=30 "$GTK4_URL"        -O  ./"gtk4-mini-$PKG_TYPE"
-if [ "$ARCH" = 'x86_64' ]; then
-  wget --retry-connrefused --tries=30 "$INTEL_MEDIA_DRIVER_URL"  -O ./"intel-media-driver-mini-$PKG_TYPE"
-fi
+wget --retry-connrefused --tries=30 "$MESA_URL"    -O  ./"mesa-nano-$PKG_TYPE"
+wget --retry-connrefused --tries=30 "$GTK4_URL"    -O  ./"gtk4-mini-$PKG_TYPE"
 
 pacman -U --noconfirm ./*.pkg.*
 rm -f ./*.pkg.*
